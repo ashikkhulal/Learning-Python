b0094241fd1934c747a1d18cfdc5a99a356a61d5
ashikkhulal <askhulal@gmail.com>
2021-06-01 15:53:45 -0500

    If/else files added

---
+++ 5if_else_conditions2.py
@@ -0,0 +1,21 @@
+convert_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    condition_check = num_of_days > 0
+    print(type(condition_check))
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * convert_to_units} {name_of_unit}."
+    else:
+        print("You entered an invalid input. (Only positive numbers greater than zero are allowed.)")
+
+
+user_input = input("Hey user, enter the number of days and I will convert it to hours!\n")
+
+
+calculated_value = days_to_units(int(user_input))
+print(calculated_value)
+
+
+print(type("this is a class of string"))
---
+++ 6if_elif_else_conditions2.py
@@ -0,0 +1,21 @@
+convert_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    if num_of_days > 0:
+        return f"{num_of_days} days are {num_of_days * convert_to_units} {name_of_unit}."
+    elif num_of_days == 0:
+        print("you entered a 0, please enter a positive number greater than zero")
+    else:
+        print("you entered an invalid input (only positive numbers greater than zero are allowed)")
+
+
+user_input = input("hey user, enter the number of days and I will convert it to hours!\n")
+
+
+if user_input.isdigit():
+    user_input_number = int(user_input)
+    calculated_value = days_to_units(user_input_number)
+else:
+    print("your input is not a number. I cannot proceed forward!")
